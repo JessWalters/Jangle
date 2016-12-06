6c06f3991b880f01e6f3c0ba05541a6c26880133
Nathan Conroy <njconroy@iasate.edu>
2016-12-05 22:34:44 -0600

    fixed disconnect hang up

--- java_jangle/src/com/jangle/voice/VoiceChat.java
+++ java_jangle/src/com/jangle/voice/VoiceChat.java
@@ -108,18 +108,11 @@ public class VoiceChat implements Runnable {
 	public void disconnectFromVoice() {
 		stopRecieve();
 		connections.clear();
-		
-
-		if (Cl.getBroadcastStatus()) {
-			endBrodcast();
-		}
+		endBrodcast();
 		Cl.setConnectedToVocie(false);
 		Parser.sendUserStatusChange();
-		try {
-			Recieving.setReceiveBufferSize(1);
-		} catch (SocketException e) {
-		}
-		stopSpeakers();
+		
+		System.out.println("end spekers");
 	}
 
 	/**
@@ -161,8 +154,9 @@ public class VoiceChat implements Runnable {
 	 * Stop playing to the speakers.
 	 */
 	private void stopSpeakers() {
-		speakers.drain();
 		speakers.close();
+		speakers.drain();
+		
 	}
 
 	private void recieveData() {
@@ -231,6 +225,11 @@ public class VoiceChat implements Runnable {
 			speakers.write(toSpeaker, 0, toSpeaker.length);
 
 		}
+		try {
+			Recieving.setReceiveBufferSize(1);
+		} catch (SocketException e) {
+		}
+		stopSpeakers();
 	}
 
 }
